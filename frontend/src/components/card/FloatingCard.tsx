0c0f9e43a8b88e5d4e121da216812b32006b7fc5
ramsy4754 <ramsy4754@clo-guard.com>
2024-03-19 17:48:52 +0900

    [😈 DEBUG] FloatingCard bound 설정 수정하여 화면 밖으로 넘어가는 드래그 방지 설정 추가

--- frontend/src/components/card/FloatingCard.tsx
+++ frontend/src/components/card/FloatingCard.tsx
@@ -1,24 +1,41 @@
-import React, {useRef} from 'react';
+import React, {forwardRef, useEffect, useImperativeHandle, useRef, useState} from 'react';
 import Draggable from 'react-draggable';
 import './FloatingCard.css';
 
 interface FloatingCardProps {
     children: React.ReactNode;
-    ref: React.MutableRefObject<any>
 }
 
-const FloatingCard = (props: FloatingCardProps) => {
+const FloatingCard = forwardRef<HTMLDivElement, FloatingCardProps>((props, forwardedRef) => {
     const {children} = props;
+    const [bounds, setBounds] = useState({left: 0, top: 0, right: 0, bottom: 0});
+    const internalRef = useRef<HTMLDivElement>(null);
+
+    useImperativeHandle(forwardedRef, () => internalRef.current!);
+
+    useEffect(() => {
+        const updateBounds = () => {
+            const viewportWidth = window.innerWidth;
+            const viewportHeight = window.innerHeight;
+
+            setBounds({top: 0, right: viewportWidth / 2, bottom: viewportHeight * 0.95, left: -viewportWidth / 2});
+        };
+
+        window.addEventListener('resize', updateBounds);
+        updateBounds();
+
+        return () => window.removeEventListener('resize', updateBounds);
+    }, []);
 
     return (
-        <Draggable>
-            <div className="draggable-container">
+        <Draggable nodeRef={internalRef} bounds={bounds}>
+            <div className="draggable-container" ref={internalRef}>
                 <div className="floating-card">
                     {children}
                 </div>
             </div>
         </Draggable>
     );
-};
+});
 
 export default FloatingCard;
