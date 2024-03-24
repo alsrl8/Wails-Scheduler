fc13a2ad5667be927a8245c945e1c1bca4e2fb4f
smg <alsrl8@naver.com>
2024-03-25 02:11:28 +0900

    Add button click 시 modal open, modal은 antd

--- frontend/src/components/add_button/AddButton.tsx
+++ frontend/src/components/add_button/AddButton.tsx
@@ -2,20 +2,44 @@ import React, {useState} from 'react';
 import plusIcon from "../../assets/images/plus.png";
 import plusIconHover from "../../assets/images/plus_hover.png"
 import "./AddButton.css";
+import {Button, Modal} from "antd";
 
 interface AddButtonProps {
 
 }
 
 const AddButton = (props: AddButtonProps) => {
-    const [isHovered, setIsHovered] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return <div className="add-button-container"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-    >
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    return <div className="add-button-container">
         <img className="add-button" src={plusIcon} alt="Add Icon"/>
-        <img className="add-button add-button-hover" src={plusIconHover} alt="Add Icon"/>
+        <img className="add-button add-button-hover" src={plusIconHover} onClick={showModal} alt="Add Icon"/>
+        <Modal
+            title="Title"
+            open={isModalOpen}
+            onOk={handleOk}
+            footer={[
+                <Button key="submit" type="primary" onClick={handleOk}>
+                    OK
+                </Button>,
+            ]}
+            onCancel={() => setIsModalOpen(false)}
+            closable={false}
+        >
+            <p>Modal content can go here.</p>
+        </Modal>
     </div>
 
 };
