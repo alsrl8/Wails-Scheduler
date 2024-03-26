f36370ad456067e6891abe0b546378c8ecb0f08a
ramsy4754 <ramsy4754@clo-guard.com>
2024-03-26 11:47:40 +0900

    Background 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 import Container from "./components/container/Container";
+import SpaceImage from './assets/images/space.png'
 
 
 function App() {
 
+    const style= {
+        backgroundImage: `url(${SpaceImage})`
+    }
+
     return (
-        <div>
+        <div className='background' style={style}>
             <Container/>
             <div>
                 Press A+A to add new schedule.
